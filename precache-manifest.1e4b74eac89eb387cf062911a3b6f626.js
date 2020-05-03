f602d6198e7d3e8ed937445fbf724069a96e308a
lucianoleme <lucianogonzagaleme@gmail.com>
2020-05-03 20:51:47 -0300

    Updates

---
+++ precache-manifest.1e4b74eac89eb387cf062911a3b6f626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10458c399322c405219e50bf05e6803c",
+    "url": "/ReactJS_GeradorMemes/index.html"
+  },
+  {
+    "revision": "71ed97a22eefe1557425",
+    "url": "/ReactJS_GeradorMemes/static/css/main.fcaa6ffd.chunk.css"
+  },
+  {
+    "revision": "be045f02f2a56562421a",
+    "url": "/ReactJS_GeradorMemes/static/js/2.b719016b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS_GeradorMemes/static/js/2.b719016b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ed97a22eefe1557425",
+    "url": "/ReactJS_GeradorMemes/static/js/main.96de7675.chunk.js"
+  },
+  {
+    "revision": "c5f7402819ca764cf8c6",
+    "url": "/ReactJS_GeradorMemes/static/js/runtime-main.acb97446.js"
+  }
+]);
